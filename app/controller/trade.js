c00f7e761d468c9c67a16dcadff9f58b15e2a728
wxaxiaoyao <765485868@qq.com>
2018-12-07 17:07:06 +0800

    update

--- app/controller/trade.js
+++ app/controller/trade.js
@@ -9,6 +9,10 @@ const {
 	TRADE_TYPE_EXCHANGE,
 	TRADE_TYPE_PACKAGE_BUY,
 	TRADE_TYPE_LESSON_STUDY,
+
+	DISCOUNT_STATE_UNUSE,
+	DISCOUNT_STATE_USED,
+	DISCOUNT_STATE_EXPIRED,
 } = require("../core/consts.js");
 
 const Trade = class extends Controller {
@@ -18,11 +22,11 @@ const Trade = class extends Controller {
 
 	async create() {
 		const {userId} = this.authenticated();
-		const {type, goodsId, count, discount=0} = this.validate({
+		const {type, goodsId, count, discountId=0} = this.validate({
 			type: "int",                      // 交易类型  课程包购买  哈奇物品兑换
 			goodsId: "int",                   // 物品id
 			count: "int",                     // 购买量
-			discount: "int_optional",         // 优惠券id
+			discountId: "int_optional",       // 优惠券id
 		});
 
 		if (count < 0) return this.throw(400);
@@ -33,9 +37,23 @@ const Trade = class extends Controller {
 		const account = await this.model.accounts.findOne({where:{userId}}).then(o => o && o.toJSON());
 		if (!account) return this.throw(500);
 		
-		const rmb = goods.rmb * count;
-		const coin = goods.coin * count;
-		const bean = goods.bean * count;
+		const discount = await this.model.discounts.findOne({where:{id: discountId, userId, state:DISCOUNT_STATE_UNUSE}}).then(o => o && o.toJSON());
+		if (discountId && !discount) return this.throw(400, "优惠券不存在");
+
+		let rmb = goods.rmb * count;
+		let coin = goods.coin * count;
+		let bean = goods.bean * count;
+		if (discount) {
+			if (rmb < discount.rmb || coin < discount.coin || bean < discount.bean) return this.throw(400, "优惠券不满足使用条件");
+			const curtime = new Date().getTime();
+			const starttitme = new Date(discount.startDate).getTime();
+			const endtime = new Date(discount.endDate).getTime();
+			if (curtime < starttitme || curtime >= endtime) return this.throw(400, "优惠券已过期");
+
+			rmb -= discount.rewardRmb;
+			coin -= discount.rewardCoin;
+			bean -= discount.rewardBean;
+		}
 
 		if (account.rmb < rmb || account.coin < coin || account.bean < bean) return this.throw(400, "余额不足");
 		
@@ -48,6 +66,9 @@ const Trade = class extends Controller {
 
 		// 更新用户余额
 		await this.model.accounts.decrement({rmb, coin, bean}, {where: {userId}});
+		
+		// 设置已使用优惠券
+		if (discount) await this.model.discounts.update({state:DISCOUNT_STATE_USED}, {where:{id: discount.id}});
 		// 创建交易记录
 		await this.model.trades.create({
 			type, goodsId, count, discount,
@@ -57,6 +78,7 @@ const Trade = class extends Controller {
 
 		return this.success("OK");
 	}
+
 }
 
 module.exports = Trade;
