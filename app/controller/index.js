a11e002cc91834f673767c2f743d0698ae69b3b7
wxaxiaoyao <765485868@qq.com>
2018-12-07 10:42:11 +0800

    fix bug

--- app/controller/index.js
+++ app/controller/index.js
@@ -5,6 +5,12 @@ const Controller = require("../core/controller.js");
 
 class Index extends Controller {
 	async index() {
+		await this.model.query("select * from users where id in (:ids)", {
+			type: this.model.QueryTypes.SELECT,
+			replacements: {
+				ids: [1,2,1],
+			}
+		});
 		this.ctx.status = 200;
 		this.ctx.body = "hello world";
 	}
