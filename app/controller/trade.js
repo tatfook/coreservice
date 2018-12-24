aa28946114b5f3c62252821ebb7edeac33690570
wxaxiaoyao <765485868@qq.com>
2018-12-24 17:33:01 +0800

    update code

--- app/controller/trade.js
+++ app/controller/trade.js
@@ -60,7 +60,7 @@ const Trade = class extends Controller {
 		let realCoin = coin;
 		let realBean = bean;
 		
-		if (rmb > 0) {  // 验证手机验证码
+		if (rmb > 200) {  // 验证手机验证码
 			if (!user.cellphone) return this.fail(5);
 			const cache = await this.model.caches.get(user.cellphone);
 			if (!params.captcha || !cache || cache.captcha != params.captcha) return this.fail(5);
