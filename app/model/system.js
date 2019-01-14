b86d54451d4a50754bbe795be48116e4ddd9339c
wxaxiaoyao <765485868@qq.com>
2019-01-14 10:40:51 +0800

    update code

---
+++ app/model/system.js
@@ -0,0 +1,40 @@
+
+const _ = require("lodash");
+
+module.exports = app => {
+	const {
+		BIGINT,
+		INTEGER,
+		STRING,
+		TEXT,
+		BOOLEAN,
+		JSON,
+		DECIMAL,
+	} = app.Sequelize;
+
+	const model = app.model.define("systems", {
+		id: {
+			type: BIGINT,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		
+	}, {
+		underscored: false,
+		charset: "utf8mb4",
+		collate: 'utf8mb4_bin',
+	});
+
+	//model.sync({force:true}).then(() => {
+		//console.log("create table successfully");
+	//});
+
+	app.model.accounts = model;
+	return model;
+};
+
+
+
+
+
+
