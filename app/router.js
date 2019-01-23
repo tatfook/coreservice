ca8428f5d0f52955525f5530cddc95806e03ad96
wxaxiaoyao <765485868@qq.com>
2019-01-23 18:36:17 +0800

    fix bug

--- app/router.js
+++ app/router.js
@@ -160,7 +160,7 @@ module.exports = app => {
 	router.post(`${prefix}admins/:resources/query`, admin.resourcesQuery);
 	router.post(`${prefix}admins/:resources/search`, admin.search);
 	router.post(`${prefix}admins/:resources/bulk`, admin.bulkCreate);
-	//router.put(`${prefix}admins/:resources/bulk`, admin.bulkUpdate);
+	router.put(`${prefix}admins/:resources/bulk`, admin.bulkUpdate);
 	//router.delete(`${prefix}admins/:resources/bulk`, admin.bulkDestroy);
 	router.resources(`${prefix}admins/:resources`, admin);
 
