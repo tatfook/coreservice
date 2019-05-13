9623523234f1c4fb5ecfcabeec5c1b2df778a039
wxaxiaoyao <765485868@qq.com>
2019-05-13 12:28:18 +0800

    fix bug

--- test/graphql/index.js
+++ test/graphql/index.js
@@ -3,13 +3,15 @@ const { app, mock, assert  } = require('egg-mock/bootstrap');
 
 describe("graphql", () => {
 	before(async () => {
-		mock.consoleLevel('DEBUG');
+		//mock.consoleLevel('DEBUG');
+		// 初始化相关数据表
 		await app.model.users.sync({force:true});
 		await app.model.lessonOrganizations.sync({force:true});
 		await app.model.lessonOrganizationClasses.sync({force:true});
 		await app.model.lessonOrganizationClassMembers.sync({force:true});
 		await app.model.lessonOrganizationPackages.sync({force:true});
 
+		// 构建3个用户
 		await app.model.users.bulkCreate([{
 			username:"xiaoyao",
 			password:"wuxiangan",
@@ -21,20 +23,43 @@ describe("graphql", () => {
 			password:"wuxiangan",
 		}]);
 
+		// 创建机构  id = 1 
 		await app.model.lessonOrganizations.create({
 			userId:1, 
 			name:"organization",
-		})
+		});
+
+		// 添加老师和学生
+		await app.model.lessonOrganizationClassMembers.bulkCreate([
+		{
+			organizationId:1,
+			classId: 0,
+			roleId:1,
+			memberId:1,
+		},
+		{
+			organizationId:1,
+			classId: 0,
+			roleId:2,
+			memberId:2,
+		},
+		{
+			organizationId:1,
+			classId: 1,
+			roleId:3,
+			memberId:3,
+		},
+		]);
 	});
 
 	// 获取机构用户
 	it("lesson organization user", async () => {
 		const data = await app.httpRequest().post("/api/v0/graphql").send({
-			query: `query($id: Int, $name: String) {organization(id: $id, name: $name) {id, studentCount, teacherCount, count }}`,
+			query: `query($id: Int, $name: String) {organization(id: $id, name: $name) {id, studentCount, teacherCount, count, students{userId} }}`,
 			variables: {
 				id:1,
 			}
-		}).expect(200).then(res => res.body);
+		}).expect(200).then(res => res.body.data);
 		console.log(data);
 	});
 });
