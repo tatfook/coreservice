3f86546f11b68f0788c6cc0c83cbb62ca0318007
william <williamzjb@163.com>
2019-05-22 17:59:51 +0800

    fix code style problems of domain model

--- app/model/domain.js
+++ app/model/domain.js
@@ -1,108 +1,72 @@
+'use strict';
+
 module.exports = app => {
-	const {
-		BIGINT,
-		INTEGER,
-		STRING,
-		TEXT,
-		BOOLEAN,
-		JSON,
-	} = app.Sequelize;
+  const {
+    BIGINT,
+    STRING,
+  } = app.Sequelize;
 
-	const model = app.model.define("domains", {
-		id: {
-			type: BIGINT,
-			autoIncrement: true,
-			primaryKey: true,
-		},
+  const model = app.model.define('domains', {
+    id: {
+      type: BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
 
-		domain: {
-			type: STRING(32),
-			unique: true,
-			allowNull: false,
-		},
+    domain: {
+      type: STRING(32),
+      unique: true,
+      allowNull: false,
+    },
 
-		userId: {
-			type: BIGINT,
-			allowNull: false,
-		},
+    userId: {
+      type: BIGINT,
+      allowNull: false,
+    },
 
-		siteId: {
-			type: BIGINT,
-			allowNull: false,
-		},
+    siteId: {
+      type: BIGINT,
+      allowNull: false,
+    },
 
-	}, {
-		underscored: false,
-		charset: "utf8mb4",
-		collate: 'utf8mb4_bin',
-	});
+  }, {
+    underscored: false,
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_bin',
+  });
 
-	model.getById = async function(id) {
-		const sql = `select domains.*, users.username, sites.sitename 
+  model.getById = async function(id) {
+    const sql = `select domains.*, users.username, sites.sitename 
 			from domains, users, sites
 			where domains.userId = users.id and domains.siteId = sites.id 
 			and domains.id = :id`;
 
-		const list = await app.model.query(sql, {
-			type: app.model.QueryTypes.SELECT,
-			replacements:{id},
-		});
+    const list = await app.model.query(sql, {
+      type: app.model.QueryTypes.SELECT,
+      replacements: { id },
+    });
 
-		if (list.length == 1) return list[0];
+    if (list.length === 1) return list[0];
 
-		return ;
-	}
+    return;
+  };
 
-	model.getByDomain = async function(domain) {
-		const sql = `select domains.*, users.username, sites.sitename 
+  model.getByDomain = async function(domain) {
+    const sql = `select domains.*, users.username, sites.sitename 
 			from domains, users, sites
 			where domains.userId = users.id and domains.siteId = sites.id 
 			and domains.domain = :domain`;
 
-		const list = await app.model.query(sql, {
-			type: app.model.QueryTypes.SELECT,
-			replacements:{domain},
-		});
+    const list = await app.model.query(sql, {
+      type: app.model.QueryTypes.SELECT,
+      replacements: { domain },
+    });
 
-		if (list.length == 1) return list[0];
+    if (list.length === 1) return list[0];
 
-		return ;
-	}
+    return;
+  };
 
-	app.model.domains = model;
-	return model;
+  app.model.domains = model;
+  return model;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
