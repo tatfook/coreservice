ee78e3a29decb2ad86ea8dcb3b531a5d43df856f
william <williamzjb@163.com>
2019-05-22 17:40:54 +0800

    add migration of comment model; fix code style problems of comment model

---
+++ database/migrations/20190522092338-create-comment.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    const {
+      BIGINT,
+      INTEGER,
+      STRING,
+      TEXT,
+      JSON,
+    } = Sequelize;
+
+    return queryInterface.createTable('comments', {
+      id: {
+        type: BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      userId: { // 评论者
+        type: BIGINT,
+        allowNull: false,
+      },
+
+      objectType: { // 评论对象类型  0 -- 用户  1 -- 站点  2 -- 页面
+        type: INTEGER,
+        allowNull: false,
+      },
+
+      objectId: { // 评论对象id
+        type: STRING,
+        allowNull: false,
+      },
+
+      content: { // 评论内容
+        type: TEXT,
+        // defaultValue:"",
+      },
+
+      extra: {
+        type: JSON,
+        defaultValue: {},
+      },
+    }, {
+      underscored: false,
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_bin',
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('comments');
+  },
+};
