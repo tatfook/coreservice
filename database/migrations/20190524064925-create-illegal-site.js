129acabbef31e2eca224978b7a54c510a2bab38d
william <williamzjb@163.com>
2019-05-24 14:50:28 +0800

    add migration of site model

---
+++ database/migrations/20190524064925-create-illegal-site.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const tableName = 'illegalSites';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const {
+      BIGINT,
+      INTEGER,
+      STRING,
+      JSON,
+      DATE,
+    } = Sequelize;
+
+    await queryInterface.createTable(tableName, {
+      id: {
+        type: BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      userId: {
+        type: BIGINT,
+        allowNull: false,
+      },
+
+      username: {
+        type: STRING(64),
+      },
+
+      sitename: {
+        type: STRING(256),
+        allowNull: false,
+      },
+
+      displayName: {
+        type: STRING(64),
+      },
+
+      visibility: {
+        type: INTEGER, // public private
+        defaultValue: 0,
+      },
+
+      description: {
+        type: STRING(1024),
+      },
+
+      extra: {
+        type: JSON,
+        defaultValue: {},
+      },
+
+      createdAt: {
+        type: DATE,
+        allowNull: false,
+      },
+
+      updatedAt: {
+        type: DATE,
+        allowNull: false,
+      },
+
+    }, {
+      underscored: false,
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_bin',
+      indexes: [
+        {
+          unique: true,
+          fields: [ 'userId', 'sitename' ],
+        },
+      ],
+    });
+
+    return queryInterface.addIndex(
+      tableName,
+      [ 'userId', 'sitename' ],
+      { unique: true }
+    );
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable(tableName);
+  },
+};
