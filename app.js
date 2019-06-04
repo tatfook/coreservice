ef31c3a27d8588adb9f399081b52dd71f034d6dd
william <williamzjb@163.com>
2019-06-04 13:54:13 +0800

    Update migration file of userinfo file

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const git = require('./app/core/git.js');
 const gitGateway = require('./app/core/gitGateway.js');
 const model = require('./app/core/model.js');
 const ahocorasick = require('./app/core/ahocorasick.js');
-const association = require("./app/core/association.js");
+// const association = require("./app/core/association.js");
 const log = require("./app/core/log.js");
 
 module.exports = app => {
@@ -34,7 +34,7 @@ module.exports = app => {
   model(app);
   ahocorasick(app);
   log(app);
-  association(app);   // 定义模型关系
+  // association(app);   // 定义模型关系
 
   app.keepworkModel = app.model;
 
