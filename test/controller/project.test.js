787eb123ad9321ac961397691a10e9b7d182a3e5
william <williamzjb@163.com>
2019-06-12 11:21:02 +0800

    Add tests of projects controller

--- test/controller/project.test.js
+++ test/controller/project.test.js
@@ -95,4 +95,132 @@ describe('test/controller/project.test.js', () => {
         .expect(200);
     });
   });
+
+  describe('get project status', () => {
+    it('should get /projects/:id/status', async () => {
+      const project = await factory.create('Project');
+      const id = project.id;
+      const user = await project.getUsers();
+      const token = app.mock.utils.token(user);
+
+      return app.httpRequest()
+        .get(`/projects/${id}/status`)
+        .set('Accept', 'application/json')
+        .set('Authorization', token)
+        .expect(200);
+    });
+  });
+
+  describe('get project status', () => {
+    it('should get /projects/:id/status', async () => {
+      const project = await factory.create('Project');
+      const id = project.id;
+      const user = await project.getUsers();
+      const token = app.mock.utils.token(user);
+
+      return app.httpRequest()
+        .get(`/projects/${id}/status`)
+        .set('Accept', 'application/json')
+        .set('Authorization', token)
+        .expect(200);
+    });
+  });
+
+  describe('get project detail', () => {
+    it('should get /projects/:id/detail', async () => {
+      const project = await factory.create('Project');
+      const id = project.id;
+      const user = await project.getUsers();
+      const token = app.mock.utils.token(user);
+
+      const res = await app.httpRequest()
+        .get(`/projects/${id}/detail`)
+        .set('Accept', 'application/json')
+        .set('Authorization', token)
+        .expect(200);
+      assert(res.body.id === project.id);
+    });
+  });
+
+  describe('visit project page', () => {
+    it('should get /projects/:id/visit', async () => {
+      const project = await factory.create('Project');
+      const id = project.id;
+      const user = await project.getUsers();
+      const token = app.mock.utils.token(user);
+
+      await app.httpRequest()
+        .get(`/projects/${id}/visit`)
+        .set('Accept', 'application/json')
+        .set('Authorization', token)
+        .expect(200);
+      const updatedProject = await app.model.Project.getById(project.id);
+      assert(updatedProject.visit === 1);
+      assert(updatedProject.lastVisit === 1);
+    });
+  });
+
+  describe('get project detail', () => {
+    it('should get /projects/:id/detail', async () => {
+      const project = await factory.create('Project');
+      const id = project.id;
+      const user = await project.getUsers();
+      const token = app.mock.utils.token(user);
+
+      const res = await app.httpRequest()
+        .get(`/projects/${id}/detail`)
+        .set('Accept', 'application/json')
+        .set('Authorization', token)
+        .expect(200);
+      assert(res.body.id === project.id);
+    });
+  });
+
+  describe('star project', async () => {
+    let project;
+    let id;
+    let user;
+    let token;
+    it('should post /projects/:id/star', async () => {
+      project = await factory.create('Project');
+      id = project.id;
+      user = await project.getUsers();
+      token = app.mock.utils.token(user);
+      await app.httpRequest()
+        .post(`/projects/${id}/star`)
+        .set('Accept', 'application/json')
+        .set('Authorization', token)
+        .expect(200);
+      const updatedProject = await app.model.Project.getById(project.id);
+      assert(updatedProject.star === 1);
+    });
+
+    it('should get true by getting /projects/:id/star', async () => {
+      const res = await app.httpRequest()
+        .get(`/projects/${id}/star`)
+        .set('Accept', 'application/json')
+        .set('Authorization', token)
+        .expect(200);
+      assert(res.body === true);
+    });
+
+    it('should post /projects/:id/unstar', async () => {
+      await app.httpRequest()
+        .post(`/projects/${id}/unstar`)
+        .set('Accept', 'application/json')
+        .set('Authorization', token)
+        .expect(200);
+      const updatedProject = await app.model.Project.getById(project.id);
+      assert(updatedProject.star === 0);
+    });
+
+    it('should get false by getting /projects/:id/star', async () => {
+      const res = await app.httpRequest()
+        .get(`/projects/${id}/star`)
+        .set('Accept', 'application/json')
+        .set('Authorization', token)
+        .expect(200);
+      assert(res.body === false);
+    });
+  });
 });
