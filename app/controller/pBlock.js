0005786048d7f0047c53fb8af484a22efe356196
wxaxiaoyao <765485868@qq.com>
2019-07-04 14:54:09 +0800

    update code

--- app/controller/pBlock.js
+++ app/controller/pBlock.js
@@ -19,6 +19,32 @@ const PBlock = class extends Controller {
 
 		return this.success();
 	}
+
+	async destroy() {
+		const {userId} = this.authenticated();
+		const {id} = this.validate({id:"number"});
+		const config = this.app.config.self;
+
+		const block = await this.model.pBlocks.findOne({where:{id, userId}}).then(o => o && o.toJSON());
+		if (!block) return this.success();
+
+		const bucketDomain = config.qiniuPublic.bucketDomain;
+		const bucket = config.qiniuPublic.bucketName;
+
+		if (block.previewUrl) {
+			const key = block.previewUrl.replace(bucketDomain + "/", "");
+			await this.ctx.service.qiniu.delete({key, bucket});
+		}
+
+		if (block.fileUrl) {
+			const key = block.fileUrl.replace(bucketDomain + "/", "");
+			await this.ctx.service.qiniu.delete({key, bucket});
+		}
+
+		await this.model.pBlocks.destroy({where:{id, userId}});
+
+		return this.success();
+	}
 }
 
 module.exports = PBlock;
