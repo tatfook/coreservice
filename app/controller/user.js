25051b9cf92d75b02d399dc4fe355301255cc2f4
wxaxiaoyao <765485868@qq.com>
2019-08-06 13:53:07 +0800

    update

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,4 +1,5 @@
 const joi = require("joi");
+const jwt = require('jwt-simple');
 const axios = require("axios");
 const uuidv1 = require('uuid/v1');
 const moment = require("moment");
@@ -67,8 +68,14 @@ const User = class extends Controller {
 		const {ctx, model} = this;
 		const {id} = this.validate();
 
-		//const user = await this.ctx.service.user.getUser({userId: id, username:id});
-		const user = await this.ctx.service.user.getUser({username:id});
+		let payload = {};
+		try {
+			payload = jwt.decode(["", id, ""].join("."), "keepwork", true);
+		} catch(e) {
+			return this.throw(400);
+		}
+		const user = await this.ctx.service.user.getUser({userId: payload.userId, username: payload.username});
+		//const user = await this.ctx.service.user.getUser({username:id});
 		if (!user) return this.throw(404);
 
 		//user.info = await this.ctx.service.user.getUserinfoByUserId(user.id);
@@ -585,8 +592,16 @@ const User = class extends Controller {
 		//const {id} = this.validate({id:'int'});
 		//const user = await this.model.users.getById(id);
 		const {id, username} = this.validate();
+
+		let payload = {};
+		try {
+			payload = jwt.decode(["", id, ""].join("."), "keepwork", true);
+		} catch(e) {
+			return this.throw(400);
+		}
+		const user = await this.ctx.service.user.getUser({userId: payload.userId, username: payload.username});
 		//const user = username ? await this.model.users.getByName(username) : await this.model.users.get(id);
-		const user = await this.ctx.service.user.getUser({username: username || id});
+		//const user = await this.ctx.service.user.getUser({username: username || id});
 		if (!user) this.throw(400);
 
 		const userId = user.id;
