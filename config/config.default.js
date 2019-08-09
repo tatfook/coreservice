506c069ada8ad0cac4e7e3ea0e5b3a1215d053dd
wxaxiaoyao <765485868@qq.com>
2019-08-09 17:02:04 +0800

    update code

--- config/config.default.js
+++ config/config.default.js
@@ -24,7 +24,8 @@ exports.security = {
 
 exports.onerror = {
 	all: (e, ctx) => {
-		const message = ctx.app.config.self.env == "prod" ? e.toString() : e.stack || e.message || e.toString();
+		//const message = ctx.app.config.self.env == "prod" ? e.toString() : e.stack || e.message || e.toString();
+		const message = ctx.app.config.self.env == "prod" ? "请求不合法" : e.stack || e.message || e.toString();
 
 		ctx.status = e.status || 500;
 		ctx.body = message;
