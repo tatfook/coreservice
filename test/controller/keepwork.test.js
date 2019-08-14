20bae4a3396055d30937a6c66ea43039d29676ce
jacksparrow68 <954457807@qq.com>
2019-08-14 10:40:43 +0800

    单元测试

--- test/controller/keepwork.test.js
+++ test/controller/keepwork.test.js
@@ -1,4 +1,4 @@
-
+const md5 = require("blueimp-md5");
 const { app, mock, assert  } = require('egg-mock/bootstrap');
 
 describe("KEEPWORK", () => {
@@ -20,10 +20,38 @@ describe("KEEPWORK", () => {
 		await app.redis.set("www.baidu.com-page-visit-count", 0);
 		// 增加页面访问量
 		let count = await app.httpRequest().post("/api/v0/keepworks/page_visit").send({ url:"www.baidu.com" }).expect(res => res.statusCode === 200).then(res => res.body);
-		assert(count == 1);
+		assert(count === 1);
 
 		// 获取页面访问量
-		count = await app.httpRequest().get("/api/v0/keepworks/page_visit?url=www.baidu.com").expect(res => res.statusCode == 200).then(res => res.text);
-		assert(count == "1");
+		count = await app.httpRequest().get("/api/v0/keepworks/page_visit?url=www.baidu.com").expect(res => res.statusCode === 200).then(res => res.text);
+		assert(count === "1");
+
+		// 增加paracraft下载量
+		await app.redis.set("paracraft_download_count",0);
+		let downLoad = await app.httpRequest().post("/api/v0/keepworks/paracraft_download_count").expect(res=>res.statusCode === 200).then(res=> res.body);
+		assert(downLoad === 1);
+
+		// 获取paracraft下载量
+		downLoad = await app.httpRequest().get("/api/v0/keepworks/paracraft_download_count").expect(res => res.statusCode === 200).then(res=>res.text);
+		assert(downLoad === "1");
+
+		// 创建用户
+		await app.factory.createMany("users", 10);
+		await app.model.admins.create({username:"admin001", password: md5("123456")}).then(o => o.toJSON());
+		// 登录
+		let user = await app.httpRequest().post("/api/v0/admins/login").send({username:"admin001", password:"123456"}).expect(res => assert(res.statusCode == 200)).then(res => res.body);
+		assert(user.token);
+		const token = user.token;
+	
+		// 设置paracraft下载地址
+		let downLoadUrl = await app.httpRequest().post("/api/v0/keepworks/paracraft_download_url")
+		.send({url:"www.bilibili.com"}).set("Authorization", `Bearer ${token}`)
+		.expect(res => res.statusCode === 200).then(res => res.text);
+
+		assert(downLoadUrl === "OK");
+
+		// 获取paracraft下载地址
+		downLoadUrl = await app.httpRequest().get("/api/v0/keepworks/paracraft_download_url").expect(res => res.statusCode ===200).then(res => res.body);
+		assert(downLoadUrl.url === "www.bilibili.com");
 	});
 });
