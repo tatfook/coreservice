5661830c8bc2e1a55d605640763d74073a78fd95
Owen <253065903@qq.com>
2019-10-22 16:08:19 +0800

    modify app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-// require('newrelic');
+require('newrelic');
 // const _ = require('lodash');
 const cache = require('memory-cache');
 const consts = require('./app/core/consts.js');
