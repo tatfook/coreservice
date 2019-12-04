1daa02cefadf5dd7935d18ad8f83f738554a2fdf
Owen <253065903@qq.com>
2019-12-04 14:29:57 +0800

    增加默认权限

--- app/service/resource/pBlocks.js
+++ app/service/resource/pBlocks.js
@@ -22,15 +22,19 @@ class PBlocks extends Model {
     }
 
     async setBlockAccess(data) {
-        if (!data.id || !data.pBlockAccesses) return;
-
+        const fields = [ 'vip', 'commonUser', 't1', 't2', 't3', 't4', 't5' ];
+        if (!data.id) return;
+        // 无权限传过来则添加默认权限
+        if (!data.pBlockAccesses) {
+            data.pBlockAccesses = {};
+            fields.forEach(field => {
+                data.pBlockAccesses[field] = 2;
+            });
+        }
         await this.app.model.pBlockAccesses.destroy({
             where: { pBlockId: data.id },
         });
-        const opts = _.pick(
-            data.pBlockAccesses,
-            ...[ 'vip', 'commonUser', 't1', 't2', 't3', 't4', 't5' ]
-        );
+        const opts = _.pick(data.pBlockAccesses, ...fields);
         opts.pBlockId = data.id;
         await this.app.model.pBlockAccesses.create(opts);
     }
--- app/service/resource/pClassifies.js
+++ app/service/resource/pClassifies.js
@@ -1,17 +1,22 @@
 'use strict';
 const Model = require('./model.js');
 const _ = require('lodash');
+const fields = [ 'vip', 'commonUser', 't1', 't2', 't3', 't4', 't5' ];
 class PBlocks extends Model {
     async setClassifyAccess(data) {
-        if (!data.id || !data.pClassifyAccesses) return;
+        if (!data.id) return;
+        // 无权限传过来则添加默认权限
+        if (!data.pClassifyAccesses) {
+            data.pClassifyAccesses = {};
+            fields.forEach(field => {
+                data.pClassifyAccesses[field] = 1;
+            });
+        }
 
         await this.app.model.pClassifyAccesses.destroy({
             where: { pClassifyId: data.id },
         });
-        const opts = _.pick(
-            data.pClassifyAccesses,
-            ...[ 'vip', 'commonUser', 't1', 't2', 't3', 't4', 't5' ]
-        );
+        const opts = _.pick(data.pClassifyAccesses, ...fields);
         opts.pClassifyId = data.id;
         await this.app.model.pClassifyAccesses.create(opts);
     }
